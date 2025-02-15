c688d8261f96696a42d32f8439b442989eb96199
Matias Tillerias <matias.tillerias@owasp.org>
2025-02-15 02:36:41 -0300

    fix: :camera_flash: cmabio de version en setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chileanfakeinfo",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["example"]),
     install_requires=[],
     author="Matias Tillerias",
